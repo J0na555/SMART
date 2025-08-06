342723fd0f680b269f6924805937fb6ac8b5fce4
J0na555 <ethiopediaa@gmail.com>
2025-08-07 01:20:23 +0300

    added an attedance model

--- tracker/models.py
+++ tracker/models.py
@@ -127,8 +127,27 @@ class Mark(models.Model):
     student = models.ForeignKey(Student, on_delete= models.CASCADE)
     assessment = models.ForeignKey(Assessment, on_delete= models.CASCADE)
 
-    
 
-    
     def __str__(self):
         return str(self.score)
+
+
+class Attendace(models.Model):
+    STATUS_CHOICES = [
+        ('present', 'present'), 
+        ('absent', 'absent'),
+        ('excused', 'excused'),
+        ('late', 'late')
+    ]
+
+    student = models.ForeignKey(Student, on_delete= models.CASCADE)
+    # Subject = models.ForeignKey(Subject, on_delete= models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(max_length=20, choices= STATUS_CHOICES)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['student',  'date'], name= 'unique_take')
+        ]
+    def __str__(self):
+        return f"{self.student.first_name} {self.student.last_name} : {self.date} - {self.status}"
